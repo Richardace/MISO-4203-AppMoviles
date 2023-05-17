ee334862c6e768fc683a058dc3b19087ec3bf94f
LeidyBeltranRomero <lt.beltranr1@uniandes.edu.co>
2023-05-17 18:21:07 -0500

    crear album

---
+++ app/src/main/java/com/moviles/vinilos/ui/CrearAlbumFragment.kt
@@ -0,0 +1,109 @@
+package com.moviles.vinilos.ui
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import com.moviles.vinilos.R
+import com.moviles.vinilos.viewmodels.crearAlbumVM
+
+
+class CrearAlbumFragment : Fragment() {
+
+    lateinit var viewModel: crearAlbumVM
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_crear_album, container, false)
+        val myButton = view.findViewById<Button>(R.id.SaveAlbumbutton)
+        val name = view.findViewById<EditText>(R.id.nombreAlbum)
+        val cover = view.findViewById<EditText>(R.id.cover)
+        val description = view.findViewById<EditText>(R.id.descripcionAlbum)
+        val releaseDate = view.findViewById<EditText>(R.id.fechaLazamiento)
+        val genre = view.findViewById<EditText>(R.id.genero)
+        val recordLabel = view.findViewById<EditText>(R.id.selloDiscografico)
+
+        name.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable) {
+                viewModel.name.value = s.toString()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        });
+
+        cover.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable) {
+                viewModel.cover.value = s.toString()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        });
+
+        description.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable) {
+                viewModel.description.value = s.toString()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        });
+
+        releaseDate.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable) {
+                viewModel.releaseDate.value = s.toString()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        });
+
+        releaseDate.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable) {
+                viewModel.releaseDate.value = s.toString()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        });
+
+        genre.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable) {
+                viewModel.genre.value = s.toString()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        });
+
+        recordLabel.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable) {
+                viewModel.recordLabel.value = s.toString()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        });
+
+        myButton.setOnClickListener {
+            viewModel.saveAlbumOnApi()
+        }
+
+        val backButton = view.findViewById<Button>(R.id.backButton)
+        backButton.setPadding(40, 40, 40, 40)
+        backButton.setOnClickListener {
+            view.findNavController().navigateUp()
+        }
+        return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val activity = requireNotNull(this.activity) {
+            "You can only access the viewModel after onActivityCreated()"
+        }
+        viewModel =
+            ViewModelProvider(this, crearAlbumVM.Factory(activity.application))[crearAlbumVM::class.java]
+
+    }
+}
--- app/src/main/java/com/moviles/vinilos/viewmodels/crearAlbumVM.kt
+++ app/src/main/java/com/moviles/vinilos/viewmodels/crearAlbumVM.kt
@@ -11,52 +11,55 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.json.JSONObject
 
-class AddArtistVM(application: Application) : AndroidViewModel(application) {
+class crearAlbumVM(application: Application) : AndroidViewModel(application){
     private val _bands = MutableLiveData<List<BandModel>>()
     private var _eventNetworkError = MutableLiveData<Boolean>(false)
     private var _isNetworkErrorShown = MutableLiveData<Boolean>(false)
     private val bandRepository = BandRepository(application)
     private var bandsList = emptyList<BandModel>()
 
-    val bands: LiveData<List<BandModel>> get() = _bands
     val eventNetworkError: LiveData<Boolean> get() = _eventNetworkError
     val isNetworkErrorShown: LiveData<Boolean> get() = _isNetworkErrorShown
-    val nameLD = MutableLiveData<String>();
-    val imageLD = MutableLiveData<String>();
-    val descriptionLD = MutableLiveData<String>();
-    val birthdateLD = MutableLiveData<String>();
-
+    val name = MutableLiveData<String>();
+    val cover = MutableLiveData<String>();
+    val releaseDate = MutableLiveData<String>();
+    val description = MutableLiveData<String>();
+    val genre = MutableLiveData<String>();
+    val recordLabel = MutableLiveData<String>();
 
     init {
         refreshDataFromNetwork()
     }
 
-    fun saveArtistOnApi() {
-
+    fun saveAlbumOnApi() {
         var canCallApi = false
         val jsonObject = JSONObject()
-        jsonObject.put("name", nameLD.value)
-        jsonObject.put("image", imageLD.value)
-        jsonObject.put("description", descriptionLD.value)
-        jsonObject.put("birthDate", birthdateLD.value)
+        jsonObject.put("name", name.value)
+        jsonObject.put("cover", cover.value)
+        jsonObject.put("releaseDate", releaseDate.value)
+        jsonObject.put("description", description.value)
+        jsonObject.put("genre", genre.value)
+        jsonObject.put("recordLabel", recordLabel.value)
 
-
-        if (nameLD.value?.isEmpty() != false) {
+        if (name.value?.isEmpty() != false) {
             Toast.makeText(getApplication(), "Campo Nombre vacío", Toast.LENGTH_LONG).show();
-        } else if (checkNameCreated(nameLD.value ?: "")) {
-            Toast.makeText(getApplication(), "El artista ya existe", Toast.LENGTH_LONG).show();
-        } else if (imageLD.value?.isEmpty() != false) {
+        } else if (checkNameCreated(name.value ?: "")) {
+            Toast.makeText(getApplication(), "El Album ya existe", Toast.LENGTH_LONG).show();
+        } else if (cover.value?.isEmpty() != false) {
             Toast.makeText(getApplication(), "Campo imagen vacío", Toast.LENGTH_LONG).show();
-        } else if (descriptionLD.value?.isEmpty() != false) {
+        } else if (releaseDate.value?.isEmpty() != false) {
+            Toast.makeText(getApplication(),"Campo fecha de lazamiento vacío",android.widget.Toast.LENGTH_LONG
+            ).show();
+        } else if (description.value?.isEmpty() != false) {
             Toast.makeText(
                 getApplication(),
-                "Campo descripción vacío",
+                "Campo descripción vacio",
                 android.widget.Toast.LENGTH_LONG
             ).show();
-        } else if (birthdateLD.value?.isEmpty() != false) {
+        }else if (recordLabel.value?.isEmpty() != false) {
             Toast.makeText(
                 getApplication(),
-                "Campo fecha de nacimiento vacío",
+                "Campo sello discografico vacio",
                 android.widget.Toast.LENGTH_LONG
             ).show();
         } else {
@@ -67,7 +70,7 @@ class AddArtistVM(application: Application) : AndroidViewModel(application) {
             bandRepository.createBand(jsonObject, {
                 Toast.makeText(
                     getApplication(),
-                    "Artista creado con exito",
+                    "Album creado con exito",
                     android.widget.Toast.LENGTH_LONG
                 ).show();
                 _eventNetworkError.value = false
@@ -90,7 +93,6 @@ class AddArtistVM(application: Application) : AndroidViewModel(application) {
     fun onNetworkErrorShown() {
         _isNetworkErrorShown.value = true
     }
-
     private fun refreshDataFromNetwork() {
         try{
             viewModelScope.launch(Dispatchers.Default){
@@ -106,6 +108,7 @@ class AddArtistVM(application: Application) : AndroidViewModel(application) {
         }
     }
 
+
     class Factory(val app: Application) : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
             if (modelClass.isAssignableFrom(AddArtistVM::class.java)) {
@@ -115,4 +118,6 @@ class AddArtistVM(application: Application) : AndroidViewModel(application) {
             throw IllegalArgumentException("Unable to construct viewmodel")
         }
     }
+
+
 }
